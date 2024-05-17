80c070106972b47d70dfbd2622785e669e4d89df
eBoeh <boehlinge26@gmail.com>
2024-05-17 17:38:13 -0400

    Created empty Django app

--- sampleSite/asgi.py
+++ sampleSite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for sampleSiteDel project.
+ASGI config for sampleSite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sampleSiteDel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sampleSite.settings')
 
 application = get_asgi_application()
--- sampleSite/wsgi.py
+++ sampleSite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sampleSiteDel project.
+WSGI config for sampleSite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sampleSiteDel.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sampleSite.settings')
 
 application = get_wsgi_application()
